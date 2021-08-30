ba0efb4566bb23a1c085871ccada93a4f1b1bea7
jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-08-30 23:41:33 +0200

    fixed script

--- download_faces.sh
+++ download_faces.sh
@@ -5,9 +5,9 @@ mkdir $DIRNAME
 for i in {0..10000..1}
 do
   wget -q -O tmp.jpeg https://thispersondoesnotexist.com/image
-  convert tmp.jpeg -resize 80x80! "$DIRNAME/$i.jpeg"
-  convert "$DIRNAME/$i" -bordercolor white 20x20 "$DIRNAME/$i"
+  convert tmp.jpeg -resize 80x80! "$DIRNAME/$i"
+  #convert "$DIRNAME/$i" -bordercolor white -border 20x20 "$DIRNAME/$i"
   echo "Wrote $i / $1"
 done
 
-mkdir beaches && cd beaches && wget -w 3 -O beaches/hello https://thisbeachdoesnotexist.com/data/seeds-075/{1..9999}.jpg
+mkdir beaches && cd beaches && wget -q -w 3 https://thisbeachdoesnotexist.com/data/seeds-075/{1..9999}.jpg

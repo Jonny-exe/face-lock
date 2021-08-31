ed93a31a39026328078ec00ef42ffdb3f8432dd7
jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-08-31 16:55:42 +0200

    added more output nodes and changed output format

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as mpl
 
 
 FACES_DIR = "newfaces"
-DATA_AMOUNT = 9000
+DATA_AMOUNT = 10000
 IMG_WIDTH = 100
 IMG_HEIGHT = 100
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -46,7 +46,7 @@ class Net(nn.Module):
         self.to_linear = None
         x = torch.randn(100, 100).view(-1, 1, 100, 100)
         self.convs(x)
-        self.last = nn.Linear(self.to_linear, 4)
+        self.last = nn.Linear(self.to_linear, 100)
 
     def convs(self, x):
         x = F.relu(self.a1(x))
@@ -75,10 +75,18 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.convs(x)
         x = x.view(-1, self.to_linear)
-        x = self.last(x)
 
-        return F.softmax(x, dim=1)
+        x1 = self.last(x)
+        x2 = self.last(x)
+        x3 = self.last(x)
+        x4 = self.last(x)
 
+        x1 = F.softmax(x1, dim=1)
+        x2 = F.softmax(x2, dim=1)
+        x3 = F.softmax(x3, dim=1)
+        x4 = F.softmax(x4, dim=1)
+
+        return [x1, x2, x3, x4]
 
 class Data:
     def __init__(self, path="training_data.npy", BATCH_SIZE=100, REMAKE_DATA=False):
@@ -86,6 +94,7 @@ class Data:
             self.make_training_data()
         try:
             self.training_data = np.load(path, allow_pickle=True)
+            np.random.shuffle(self.training_data)
         except FileNotFoundError:
             self.training_data = self.make_training_data()
 
@@ -94,16 +103,22 @@ class Data:
 
     def make_training_data(self):
         training_data = []
+
         idx = 0
         for filename in tqdm(os.listdir(FACES_DIR)):
             f = os.path.join(FACES_DIR, filename)
             image = MyImage(f)
+            if idx == 0:
+                image.show_image()
             if len(image.pixels) != 100:
                 break
 
             pieces = [list(map(int, i.split("x"))) for i in filename.split("X")]
-            result = [pieces[0][0], pieces[0][1], pieces[1][0], pieces[1][1]]
+            eye = np.eye(100)
+            result = [eye[pieces[0][0]], eye[pieces[0][1]], eye[pieces[1][0]], eye[pieces[1][1]]]
+
             training_data.append((image.pixels, result))
+
             idx += 1
 
             if idx == DATA_AMOUNT:
@@ -116,9 +131,10 @@ class Data:
     def format(self):
         X = torch.Tensor([i[0] for i in self.training_data]).view(-1, 1, 100, 100).to(DEVICE)
         X /= 255.0
+        # person type data  type people data
+        #   100    4  100 ->  4   100   100
         Y = torch.Tensor([i[1] for i in self.training_data]).to(DEVICE)
-        Y /= 100.0
-        print(Y.device)
+        # Y = [[y[i][k] for k in range(len(y[i]))] for i in range(len(y[0]))]
 
         self.train_X = X[self.BATCH_SIZE :]
         self.test_X = X[: self.BATCH_SIZE]
@@ -128,7 +144,7 @@ class Data:
 
 
 class TrainModel:
-    def __init__(self, net, data, EPOCHS=3, BATCH_SIZE=100):
+    def __init__(self, net, data, EPOCHS=13, BATCH_SIZE=100):
         self.EPOCHS = EPOCHS
         self.BATCH_SIZE = BATCH_SIZE
 
@@ -137,8 +153,9 @@ class TrainModel:
 
         self.data = data
 
-        self.optimizer = optim.Adam(self.net.parameters(), lr=0.001)
-        self.loss_function = nn.MSELoss()
+        self.optimizer = optim.Adam(self.net.parameters(), lr=0.01)
+        # self.optimizer = optim.SGD(self.net.parameters(), lr=0.01, momentum=0.9)
+        self.loss_functions = [nn.MSELoss() for _ in range(4)]
 
         self.train()
         try:
@@ -151,37 +168,25 @@ class TrainModel:
 
     def train(self):
         for epoch in tqdm(range(self.EPOCHS)):
-            for i in tqdm(range(0, len(self.data.train_Y), self.BATCH_SIZE)):
+            for i in range(0, len(self.data.train_Y), self.BATCH_SIZE):
                 batch_X = self.data.train_X[i : i + self.BATCH_SIZE].view(
                     -1, 1, 100, 100
                 )
+
                 batch_Y = self.data.train_Y[i : i + self.BATCH_SIZE]
-                # batch_Y, batch_X = batch_Y.cuda(), batch_X.cuda()
+                # Convert form from -1, 4, 100 --> 4, -1, 100. Can't use view because you distroy the order
+                batch_Y = torch.Tensor([[batch_Y[j][k].tolist() for j in range(len(batch_Y))] for k in range(len(batch_Y[0]))]).to(DEVICE) 
 
                 self.net.zero_grad()
-                outputs = self.net(batch_X)
+                outputs = self.net(batch_X) # Shape: (4, -1, 100)
 
-                loss = self.loss_function(outputs, batch_Y)
+                loss = 0
+
+                for j in range(len(outputs)):
+                    loss += self.loss_functions[j](outputs[j], batch_Y[j])
+
                 loss.backward()
                 self.optimizer.step()
-            print("Loss: ", loss)
-
-
-class MyImage:
-    def __init__(self, image_path):
-        self.path = image_path
-        self.get_image_pixels()
-
-    def get_image_pixels(self):
-        i = Image.open(self.path).convert("L")
-        p = i.load()
-        self.width, self.height = i.size
-        self.pixels = [[0 for _ in range(self.width)] for _ in range(self.height)]
-
-        for x in range(self.width):
-            for y in range(self.height):
-                cpixel = p[x, y]
-                self.pixels[y][x] = cpixel
 
 
 if __name__ == "__main__":
@@ -195,9 +200,15 @@ if __name__ == "__main__":
 
     with torch.no_grad():
         BATCH_SIZE = 100
-        batch_X = data.train_X[:BATCH_SIZE].view(-1, 1, 100, 100)
-        batch_Y = data.train_Y[:BATCH_SIZE] * 100
-
-        network = net(batch_X) * 100
-        for i in tqdm(range(100)):
-            print(f"Expected: {batch_Y[i]}\nNetwork: {network[i]}")
+        batch_X = data.test_X[:BATCH_SIZE].view(-1, 1, 100, 100)
+        batch_Y = data.test_Y[:BATCH_SIZE]
+        # Convert form from -1, 4, 100 --> 4, -1, 100. Can't use view because you distroy the order
+        batch_Y = torch.Tensor([[batch_Y[j][k].tolist() for j in range(len(batch_Y))] for k in range(len(batch_Y[0]))]).to(DEVICE) 
+        network = net(batch_X)
+        print(batch_Y)
+        print(network)
+        for i in range(4):
+            print("##########################", i," ####################")
+            for j in range(2):
+                print(f"Network  Argmax: {torch.argmax(network[i][j])}")
+                print(f"Expected Argmax: {torch.argmax(batch_Y[i][j])}")

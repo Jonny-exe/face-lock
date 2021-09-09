1039d2df3d5a4c78e1001e0158c4680a815bfd95
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-09-09 17:11:39 +0200

    added function to check if data is balanced

---
+++ balance_data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import os
+FACES_DIR = "newfaces"
+def check_data_is_balanced():
+    data_sum = [0, 0, 0, 0]
+    idx = 0
+    for filename in os.listdir(FACES_DIR):
+        # f = os.path.join(FACES_DIR, filename)
+        data = [x.split("x") for x in filename.split("X")]
+        data = [data[0][0], data[0][1], data[1][0], data[1][1]]
+        data = list(map(int, data))
+
+        for i in range(4):
+            data_sum[i] += data[i]
+        idx += 1
+
+    for i in range(4):
+        data_sum[i] /= idx
+    return data_sum
+    
+if __name__ == "__main__":
+    mean_data = check_data_is_balanced()
+    print(mean_data)
+
+
+
